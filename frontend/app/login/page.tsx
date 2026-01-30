13a302d9c0697df5dc41cff5987b66fbe491d194
Daksh Patel <dakshpatel1700@gmail.com>
2026-01-30 13:48:25 -0500

    General UI Changes to Auth Pages Made

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-8 gap-4">
+      <h1 className="text-4xl font-bold">KeepFresh Login</h1>
+      <p className="mt-4">Login Below</p>
+    </div>
+  );
+}
---
+++ frontend/app/signup/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+import { useState } from 'react';
+
+export default function SignUp() {
+  const [username, setUsername ] = useState('');
+  const [password, setPassword ] = useState('');
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  }
+
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-8 gap-4">
+      <h1 className="text-4xl font-bold">KeepFresh Sign Up</h1>
+      <p className="mt-4">Sign Up Below!</p>
+      <form onSubmit={handleSubmit} className="min-h-screen p-8 flex flex-col items-center justify-center gap-4">
+        <input
+          type="text"
+          placeholder="Enter Username: "
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}     
+          className="p-8"
+        />
+        <input
+          type="password"
+          placeholder="Enter Password: "
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}    
+          className="p-8" 
+        />
+        <button
+          type="submit"
+          className="bg-blue-600 text-white"
+        >
+          Sign Up
+        </button>
+
+      </form>
+    </div>
+  );
+}
