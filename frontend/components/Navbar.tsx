79db299a07c94c2591000a3fbff594ff60e1242d
svastik <svastiksharma13@gmail.com>
2026-02-19 09:00:15 -0500

    supporting components ui upgrade

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,14 +1,27 @@
 import Link from "next/link";
+import { AppBar, Button, Stack, Toolbar, Typography } from "@mui/material";
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-green-600 text-white p-4 flex justify-center gap-6">
-      <Link href="/login" className="hover:underline">
-        Login
-      </Link>
-      <Link href="/signup" className="hover:underline">
-        Sign Up
-      </Link>
-    </nav>
+    <AppBar position="static" color="primary" elevation={2}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Typography variant="h6" component="div" fontWeight={700}>
+          KeepFresh
+        </Typography>
+        <Stack direction="row" spacing={1}>
+          <Button component={Link} href="/login" color="inherit">
+            Login
+          </Button>
+          <Button
+            component={Link}
+            href="/signup"
+            variant="outlined"
+            color="inherit"
+          >
+            Sign Up
+          </Button>
+        </Stack>
+      </Toolbar>
+    </AppBar>
   );
 }
